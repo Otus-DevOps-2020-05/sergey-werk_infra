f6ab19763f877b28a419fe2387f088311a974775
Sergey Ryzhikov <ryzhikov@ihep.ru>
2020-07-13 23:16:25 +0300

    multiple bugfixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 # Install MondoDB
-wget -qO -- "https://www.mongodb.org/static/pgp/server-4.2.asc" | sudo apt-key add -
+wget -qO - "https://www.mongodb.org/static/pgp/server-4.2.asc" | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee "/etc/apt/sources.list.d/mongodb-org-4.2.list"
-sudo apt update && apt install -y mongodb-org
+sudo apt-get update && sudo apt-get install -y mongodb-org
 sudo systemctl start mongod && sudo systemctl enable mongod
 
 # Install ruby
-sudo apt update && apt install -y ruby-full ruby-bundler build-essential
+sudo apt-get update && sudo apt-get install -y ruby-full ruby-bundler build-essential
 
 # Deploy
+sudo apt-get install -y git
 cd ~
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 
-# Run Dev Version
-sudo cat <<EOT >>/etc/systemd/system/reddit.service
+# Install and run the Dev. version
+sudo tee <<EOT >>/etc/systemd/system/reddit.service
 [Unit]
 Description=Puma HTTP Server
 After=network.target
@@ -32,4 +33,5 @@ Restart=always
 WantedBy=multi-user.target
 EOT
 
+sudo systemctl daemon-reload
 sudo systemctl start reddit.service && sudo systemctl enable reddit.service
