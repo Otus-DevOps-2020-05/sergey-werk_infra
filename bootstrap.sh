2f4f67d1d63a6c32140da5b995d03763fc08024d
Sergey Ryzhikov <ryzhikov@ihep.ru>
2020-07-13 23:54:38 +0300

    fix bootstrap: cd to /home/appuser

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+su appuser
+cd ~
+
 # Install MondoDB
 wget -qO - "https://www.mongodb.org/static/pgp/server-4.2.asc" | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee "/etc/apt/sources.list.d/mongodb-org-4.2.list"
