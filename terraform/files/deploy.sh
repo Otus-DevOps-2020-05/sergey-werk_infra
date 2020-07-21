b5cec7bf7936ae8ffd372b1b2c9947c77da25d61
Sergey Ryzhikov <ryzhikov@ihep.ru>
2020-07-22 00:53:02 +0300

    Fix: apt now is waiting for running competitiors.

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 APP_DIR=${1:-$HOME}
+
+while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+   echo Waiting for other instances of apt to complete...
+   sleep 5
+done
+
 sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
