d2b122c58d4cf5a9e8185981ecf205fcae3eacac
Sergey Ryzhikov <ryzhikov@ihep.ru>
2020-08-04 19:46:29 +0300

    commit unstaged

--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -2,7 +2,7 @@ output "external_ip_addresses_app" {
   value = module.app.external_ip_addr_app
 }
 output "external_ip_address_db" {
-  value = module.db.external_ip_addr_db
+  value = module.db.external_ip_addr
 }
 
 
--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -12,6 +12,7 @@ variable zone {
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
+
 variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
