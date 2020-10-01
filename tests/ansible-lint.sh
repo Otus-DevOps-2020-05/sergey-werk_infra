4922bfbe3297d384430727569fbb5e78fca81202
Sergey Ryzhikov <ryzhikov@ihep.ru>
2020-10-01 23:57:38 +0300

    fix ansible-lint.sh

--- tests/ansible-lint.sh
+++ tests/ansible-lint.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+set -ev
+
 echo "Ansible Linter"
-cd ansible/playbooks && ansible-lint
+cd ansible/playbooks
+for file in *.yml; do
+	ansible-lint $file --exclude=roles/jdauphant.nginx && echo $file - ok
+done
